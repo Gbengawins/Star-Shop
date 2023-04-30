b604dab5bb8eaf320792ec868d498e320b360f6f
Victor Ajagunna <victor-ajagunna@hotmail.com>
2023-04-30 15:31:34 +0200

    Routes setup

---
+++ server/routes/orders.js
@@ -0,0 +1,94 @@
+const { Order } = require("../models/OrderModel");
+const { auth, isUser, isAdmin } = require("../middleware/auth");
+
+const router = require("express").Router();
+
+//Create a new Order
+
+router.post("/", auth, async (req, res) => {
+    const newOrder = new Order(req.body);
+
+    try {
+        const savedOrder = await newOrder.save();
+        res.status(200).send(savedOrder);
+    } catch (err) {
+        res.status(500).send(err);
+    }
+});
+
+//Update order
+router.put("/id", isAdmin, async (req, res) => {
+    try {
+        const updatedOrder = await Order.findByIdAndUpdate(
+            req.params.id,
+            {
+                $set: req.body,
+            },
+            { new: true }
+        );
+        res.status(200).send(updatedOrder);
+    } catch (err) { 
+        res.status(500).send(err);
+    }
+});
+
+// Delete order
+router.delete("/:id", isAdmin, async (req, res) => {
+  try {
+    await Order.findByIdAndDelete(req.params.id);
+    res.status(200).send("Order has been deleted...");
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
+
+
+// Get user orders
+router.get("/find/:userId", isUser, async (req, res) => {
+  try {
+    const orders = await Order.find({ userId: req.params.userId });
+    res.status(200).send(orders);
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
+
+// Get all orders
+router.get("/", isAdmin, async (req, res) => {
+  try {
+    const orders = await Order.find();
+    res.status(200).send(orders);
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
+
+// Get monthly income
+router.get("/income", isAdmin, async (req, res) => {
+  const date = new Date();
+  const lastMonth = new Date(date.setMonth(date.getMonth() - 1));
+  const previousMonth = new Date(new Date().setMonth(lastMonth.getMonth() - 1));
+
+  try {
+    const income = await Order.aggregate([
+      { $match: { createdAt: { $gte: previousMonth } } },
+      {
+        $project: {
+          month: { $month: "$createdAt" },
+          sales: "$amount",
+        },
+      },
+      {
+        $group: {
+          _id: "$month",
+          total: { $sum: "$sales" },
+        },
+      },
+    ]);
+    res.status(200).send(income);
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
+
+module.exports = router;
